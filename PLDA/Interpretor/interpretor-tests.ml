0105a75cf7bf06eb4bcfd33242cd68ec6905e031
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2013-11-02 17:11:12 +0200

    Removed passed/failed status, test now output result instead.

--- PLDA/Interpretor/interpretor-tests.ml
+++ PLDA/Interpretor/interpretor-tests.ml
@@ -1,43 +1,38 @@
 
-let test x= if x==1 then print_string("passed") else print_string("failed");;
-
 (*Generic test*)
-test (run(P(
+run(P(
   [
     Let("x3",Pl(I(2),I(1)));
     Let("x6",I(6));
     Letf("f",["x3";"a"],M(Id("a"),Id("x3")));
   ],
-  If (Pl( M(I(2), Pl(I(5),Id("x6")) ) ,I(-22)),
-    I(0),
-    I(1))
-)));;
+  Pl( M(I(2), Pl(I(5),Id("x6")) ) ,I(-22))
+));;
 
 (* Redefining test*)
-test (run(P(
+run(P(
   [
     Let("x3",Pl(I(2),I(1)));
     Let("x6",I(6));
     Letf("f",["x"],Pl(I(1),Id("x")));
     Letf("f",["x"],Pl(I(2),Id("x")));
   ],
-  If((Pl(F("f",[Id("x3")]),I(-5))),
-     I(0),
-     I(1))
-)));;
+  Pl(F("f",[Id("x3")]),I(-5))
+));;
 
 (* Recursive test*)
-test (run(P(
+run(P(
   [
     Let("x3",Pl(I(2),I(1)));
     Let("x6",I(6));
-    Letfrec("f",["x"],If( 
+    Let("x4",I(4));
+    Letf("f",["x"],I(2));
+    Letfrec("f",["x"],If(
       (Pl(Id("x"),I(-4))),
       Id("x"),
       F("f",[Pl(Id("x"),I(3))])));
   ],
-  If(Pl(F("f",[I(4)]),I(-7)),
-     I(0),
-     I(1))
-)));;
+  F("f",[Id("x4")]))
+);;
+
 
