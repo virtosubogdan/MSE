05508514acaed34595d317148443de14e66d258e
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2013-11-05 19:32:40 +0200

    Working for basic tests.

--- PLDA/lambda/lambda_interpretor.ml
+++ PLDA/lambda/lambda_interpretor.ml
@@ -1,20 +1,43 @@
+(* basic working draft *)
 
-type e= V of string
-	|Def of string * e
-	|App of e * e;;
+(* no operations, just substitution *)
+(* problem: same names, solution string set*)
 
-module Env =Map.Make(String);; 
+type lambda= E of string
+	    |L of string * lambda
+	    |Ap of lambda * lambda;;
 
-let rec ev ex env app=match ex with 
-    V(name)->if(Env.mem name env) then Env.find name env else ex
-  |Def(name,ex1)-> 
-     (match app with 
-	  hd::tl->(ev ex1 (Env.add name hd env) tl)
-	|[] ->V("err"))
-  |App(ex1,ex2)->ev ex1 env (ex2::app);;
+let rec l_to_s l=match l with E(name)-> name
+  |L(name,v) -> "L("^name^","^(l_to_s v)^")"
+  |Ap(e1,e2) -> (l_to_s e1)^" "^(l_to_s e2);;
 
+module SS=Set.Make(String);;
 
-let eval ex=ev ex Env.empty [];;
+let rec substitute ex1 rname ex2=
+  Printf.printf "subst   %s in %s with %s \n" rname (l_to_s ex1) (l_to_s ex2);
+  match ex1 with
+    E(name)-> if(name=rname) then ex2 else ex1
+  |L(name,exp)-> if(name<>rname) then ex1 else (substitute exp rname ex2)
+  |Ap(exA,exB)-> Ap((substitute exA rname ex2),(substitute exB rname ex2));;
 
-(* ^p.^q.p q p *)
-eval(App(App(Def("p",Def("q",V("p"))),V("q")),V("p")));; 
+let rec replace ex1 ex2=
+  Printf.printf "replace %s with %s \n" (l_to_s ex1) (l_to_s ex2);
+  match ex1 with
+    E(_) -> ex1
+  |L(name,exDest) -> substitute exDest name ex2
+  |Ap(exA,exB) -> Ap((eval ex1),ex2);
+
+and eval lambd =
+  Printf.printf "eval    %s \n" (l_to_s lambd);
+  match lambd with 
+      E(value)->E(value);
+    |L(param,value)->L(param,value);
+    |Ap(ex1,ex2)->eval (replace ex1 ex2);;
+
+let run lambd=eval lambd;;
+
+(*  ^y.^x.x a b -> b *)
+run(Ap(Ap(L("y",L("x",E("x"))),E("a")),E("b")));;
+
+(*  ^y.^x.x a -> ^x.x *)
+run(Ap(L("y",L("x",E("x"))),E("a")));;
